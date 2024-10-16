6c8493211187e5e8b88666640f418516d520df4c
Akila Piyumal <92683626+Akiladjp@users.noreply.github.com>
2024-10-16 21:39:00 +0530

    search-bar

--- src/pages/RentCar.jsx
+++ src/pages/RentCar.jsx
@@ -3,17 +3,30 @@ import { VehicleComp } from "../components/Vehicle/VehicleComp";
 import AllCars from "../assets/documents/AllCars.js";
 
 export const RentCar = () => {
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
+  // useEffect(() => {
+  //   window.scrollTo(0, 0);
+  // }, []);
 
   return (
     <div className="pt-32 mb-12">
-      <div className="flex justify-center">
-        <h1 className="text-4xl font-extrabold text-gray-900 mb-4">
+      <div className="flex flex-col items-center justify-center relative">
+        <h1 className="text-4xl font-extrabold text-gray-900 mb-4 text-center">
           Book Your Vehicle
         </h1>
+        <div className="absolute top-0 right-40">
+          <div className="relative">
+            <input
+              type="text"
+              className="border rounded-md py-2 px-4 focus:outline-none focus:ring-2 pr-28"
+              placeholder="Search City..."
+            />
+            <button className="absolute inset-y-0 right-0 flex items-center px-3 text-gray-600 hover:text-black focus:outline-none rounded-md bg-[#ffe030]">
+              Search
+            </button>
+          </div>
+        </div>
       </div>
+      4
       <div className="flex justify-center">
         <div className="my-12 grid grid-cols-4 gap-16">
           {AllCars.map((data, index) => (
@@ -28,6 +41,15 @@ export const RentCar = () => {
             />
           ))}
         </div>
+      </div>
+      <div className="flex justify-center items-center mt-8">
+        <button className="flex items-center px-6 py-2 text-gray-600 hover:text-black focus:outline-none rounded-md bg-[#ffe030] mx-2 hover:bg-[#ffcf30]">
+          Previous Page
+        </button>
+        <span className="mx-4 text-gray-600 text-lg bg-gray-200 h-10 w-10 rounded-full">1</span>
+        <button className="flex items-center px-6 py-2 text-gray-600 hover:text-black focus:outline-none rounded-md bg-[#ffe030] mx-2 hover:bg-[#ffcf30]">
+          Next Page
+        </button>
       </div>
     </div>
   );
